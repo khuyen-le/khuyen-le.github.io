bf27339e61bee362d32d2506ab71be8851730894
khuyen-le <38027953+khuyen-le@users.noreply.github.com>
2025-07-03 07:04:54 +0000

    Deploying to gh-pages from @ khuyen-le/khuyen-le.github.io@42c7041c251fec1dcb6b4fbe05319d7036ebf325 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-research-activities",
+          title: "research activities",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/research/";
           },
         },{id: "nav-publications",
           title: "publications",
@@ -32,10 +32,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/_pages/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
